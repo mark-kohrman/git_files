673d5d2e0070d55370842c1f702fd892e3efee3c
Mark-Kohrman <mark.kohrman@gmail.com>
2020-07-08 16:19:59 -0500

    adding more exercises to convert_data.rb

--- convert_data.rb
+++ convert_data.rb
@@ -35,20 +35,59 @@ end
 p letter_frequencies
 #  4. Convert a hash into an array of arrays.
 #     For example, {"chair" => 100, "book" => 14} becomes [["chair", 100], ["book", 14]].
-
+items = {"chair" => 100, "book" => 14}
+array = []
+items.each do |item, price|
+  array << [item, price]
+end
+p array
 
 #  5. Convert a hash into an array of hashes using the keys from each hash as the :id key in each of the array's hashes.
 #     For example, {321 => {name: "Alice", age: 31}, 322 => {name: "Maria", age: 27}} becomes [{id: 321, name: "Alice", age: 31}, {id: 322, name: "Maria", age: 27}].
-
+people = {321 => {name: "Alice", age: 31}, 322 => {name: "Maria", age: 27}}
+people_array = []
+people.each do |key, value|
+  value[:id] = key
+  people_array << value
+end
+p people_array
 #  6. Convert an array of strings into a hash with keys for each string in the array and values for the number of times the string appears in the array.
 #     For example, ["do", "or", "do", "not"] becomes {"do" => 2, "or" => 1, "not" => 1}.
+words = ["do", "or", "do", "not", "light", "a", "fire", "or", "fire"]
+words_frequency = {}
+index = 0
+while index < words.length
+  word = words[index]
+  if words_frequency[word] == nil
+    words_frequency[word] = 0
+  end
+  words_frequency[word] += 1
+  index += 1
+end
+
+p words_frequency
+    
+
+
 
 #  7. Convert a hash into a flat array containing all the hash’s keys and values.
 #     For example, {"a" => 1, "b" => 2, "c" => 3, "d" => 4} becomes ["a", 1, "b", 2, "c", 3, "d", 4].
-
+hash = {"a" => 1, "b" => 2, "c" => 3, "d" => 4}
+array = []
+index = 0
+hash.each do |key, value|
+    array << key
+    array << value
+end
+p array
 #  8. Combine data from a hash with names and prices and an array of hashes with names, colors, and weights to make a new hash.
 #     For example, {"chair" => 75, "book" => 15} and [{name: "chair", color: "red", weight: 10}, {name: "book", color: "black", weight: 1}] becomes {"chair" => {price: 75, color: "red", weight: 10}, "book" => {price: 15, color: "black", weight: 1}}.
+hash = {"chair" => 75, "book" => 15} 
+array_hash = [{name: "chair", color: "red", weight: 10}, {name: "book", color: "black", weight: 1}]
 
+new_hash = {}
+index = 0
+p hash  .length + array_hash[0].length  
 #  9. Convert an array of hashes into a hash of arrays, using the author as keys and the titles as values.
 #     For example, [{author: "Jeff Smith", title: "Bone"}, {author: "George Orwell", title: "1984"}, {author: "Jeff Smith", title: "RASL"}] becomes {"Jeff Smith" => ["Bone", "RASL"], "George Orwell" => ["1984"]}.
 
