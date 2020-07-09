e8548029dcc2e83d144d1f6b0e06d8a34531d2fb
Mark-Kohrman <mark.kohrman@gmail.com>
2020-07-08 22:50:26 -0500

    adding practice assignment

--- convert_data.rb
+++ convert_data.rb
@@ -82,17 +82,45 @@ end
 p array
 #  8. Combine data from a hash with names and prices and an array of hashes with names, colors, and weights to make a new hash.
 #     For example, {"chair" => 75, "book" => 15} and [{name: "chair", color: "red", weight: 10}, {name: "book", color: "black", weight: 1}] becomes {"chair" => {price: 75, color: "red", weight: 10}, "book" => {price: 15, color: "black", weight: 1}}.
-hash = {"chair" => 75, "book" => 15} 
-array_hash = [{name: "chair", color: "red", weight: 10}, {name: "book", color: "black", weight: 1}]
-
-new_hash = {}
+price_hash = { "chair" => 75, "book" => 15 }
+items = [{ name: "chair", color: "red", weight: 10 }, { name: "book", color: "black", weight: 1 }]
+combined_hash = {}
 index = 0
-p hash  .length + array_hash[0].length  
+while index < items.length
+  item = items[index]
+  name = item[:name]
+  color = item[:color]
+  weight = item[:weight]
+  price = price_hash[name]
+  combined_hash[name] = { price: price, color: color, weight: weight }
+  index += 1
+end
+p combined_hash
 #  9. Convert an array of hashes into a hash of arrays, using the author as keys and the titles as values.
 #     For example, [{author: "Jeff Smith", title: "Bone"}, {author: "George Orwell", title: "1984"}, {author: "Jeff Smith", title: "RASL"}] becomes {"Jeff Smith" => ["Bone", "RASL"], "George Orwell" => ["1984"]}.
 
+books = [{ author: "Jeff Smith", title: "Bone" }, { author: "George Orwell", title: "1984" }, { author: "Jeff Smith", title: "RASL" }]
+books_hash = {}
+index = 0
+while index < books.length
+  book = books[index]
+  author = book[:author]
+  title = book[:title]
+  if books_hash[author] == nil
+    books_hash[author] = []
+  end
+  books_hash[author] << title
+  index += 1
+end
+p books_hash
 # 10. Given a hash, create a new hash that has the keys and values switched.
 #     For example, {"a" => 1, "b" => 2, "c" => 3} becomes {1 => "a", 2 => "b", 3 => "c"}.
+original_hash = { "a" => 1, "b" => 2, "c" => 3 }
+flipped_hash = {}
+original_hash.each do |key, value|
+  flipped_hash[value] = key
+end
+p flipped_hash
 
 
 # SOLUTIONS: https://gist.github.com/peterxjang/216a7a6e8411ee5c05118e78022f2bc7
