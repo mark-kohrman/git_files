73b0d3065ed6f793423b7984014de06acba14cca
Mark-Kohrman <mark.kohrman@gmail.com>
2020-07-11 13:21:53 -0500

    adding the concatenation skills practice

---
+++ concat.rb
@@ -0,0 +1,51 @@
+# 1. Write a program that uses variables to store a first and last name, then prints the full name in one line using string concatenation (the + operator).
+first_name = "BILL"
+last_name = "Benson"
+full_name = first_name +" "+ last_name
+p full_name
+# 2. Write a program that uses variables to store a first and last name, then prints the full name in one line using string interpolation (the #{} operator).
+first_name = "Bob"
+last_name = "Benson"
+full_name = "#{first_name} #{last_name}"
+p full_name
+# 3. Write a program that asks the user to input a word. If the word is "marco", print "polo".
+# puts "Please enter a word:"
+# user_input = gets.chomp
+
+# if user_input == "marco"
+#   p "polo"
+# end
+# 4. Write a program that uses variables to store three different colors, then prints out a sentence using the colors with string concatenation (the + operator).
+# color1 = "purple"
+# color2 = "yellow"
+# color3 = "red"
+# color_senctence = "Why isn't the American flag colored" + " " + color1 + ", " + color2 + ", and " + color3 + "?"
+# p color_senctence
+# 5. Write a program that uses variables to store three different colors, then prints out a sentence using the colors with string interpolation (the #{} operator).
+# color1 = "purple"
+# color2 = "yellow"
+# color3 = "green"
+# color_senctence = "Why isn't the American flag colored #{color1}, #{color2}, and #{color3}?"
+# p color_senctence
+# 6. Write a program that asks the user to enter a name. If the name is not "Santa", print "You're not Santa."
+# puts "please enter a name:"
+# name = gets.chomp
+# if name != "Santa"
+#   p "You're not Santa."
+# end
+# 7. Write a program that uses variables to store a book's title and author, then prints out a sentence using that information with string concatenation (the + operator).
+# book = "The Righteous Mind"
+# author = "Jonathan Haidt"
+# sentence = author + " wrote the book " + book
+# p sentence 
+# 8. Write a program that uses variables to store a book's title and author, then prints out a sentence using that information with string interpolation (the #{} operator).
+book = "4 Hour Work Week"
+author = "Tim Ferriss"
+sentence2 = "#{author} wrote the great and interesting book, #{book}."
+p sentence2
+# 9. Write a program that asks the user to enter a password. If the password is "Joshua", the program responds "Shall we play a game?". For any other password, the program responds "Access denied"
+
+# 10. Write a program that uses variables to store the names of three cities, then prints out a sentence using that information with string concatenation (the + operator).
+
+
+# SOLUTIONS: https://gist.github.com/peterxjang/79ba5acc912a86cce2a18c2f14b1c712
