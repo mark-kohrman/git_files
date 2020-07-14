dc50414920815046f9b68224b78368d192c93f74
Mark-Kohrman <mark.kohrman@gmail.com>
2020-07-13 22:18:23 -0500

    adding each skills practice

---
+++ test.rb
@@ -0,0 +1,18 @@
+# numbers = [4, 9, 3, 12, 3, 2, 6, 1]
+
+# max_number = 0
+# second_highest = 0
+
+# index = 0
+
+# while index < numbers.length
+#   if numbers[index] > max_number
+#      second_highest = max_number
+#      max_number = numbers[index]
+#   elsif numbers[index] > second_highest
+#      second_highest = numbers[index]
+#   end
+# index +=1
+# end 
+# p max_number * second_highest
+
