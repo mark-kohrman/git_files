953d1a65d78a72b5d64a828d45578667872d33b8
Mark-Kohrman <mark.kohrman@gmail.com>
2020-08-13 22:26:32 -0500

    adding convert practice

--- convert.rb
+++ convert.rb
@@ -46,10 +46,25 @@ end
 p string_hash
 #  4. Convert a hash into an array of arrays.
 #     For example, {"chair" => 100, "book" => 14} becomes [["chair", 100], ["book", 14]].
-
+stuff =  {"chair" => 100, "book" => 14}
+combined_array = []
+stuff.each do |key, value|
+  combined_array << [key, value]
+end
+p combined_array
 #  5. Convert a hash into an array of hashes using the keys from each hash as the :id key in each of the array's hashes.
 #     For example, {321 => {name: "Alice", age: 31}, 322 => {name: "Maria", age: 27}} becomes [{id: 321, name: "Alice", age: 31}, {id: 322, name: "Maria", age: 27}].
 
+people_hash = {321 => {name: "Alice", age: 31}, 322 => {name: "Maria", age: 27}} 
+people_array = []
+i = 0
+people_hash.each do |key, value|
+  people_array << value
+  people_array[i][:id] = key
+  i +=1
+end
+
+p people_array
 #  6. Convert an array of strings into a hash with keys for each string in the array and values for the number of times the string appears in the array.
 #     For example, ["do", "or", "do", "not"] becomes {"do" => 2, "or" => 1, "not" => 1}.
 
---
+++ fantasy_test.rb
@@ -0,0 +1,4 @@
+i = 1
+17.times do 
+csv_text = File.read(Rails.root.join('lib', 'seeds', '2019week #{i}.csv'))
+end
