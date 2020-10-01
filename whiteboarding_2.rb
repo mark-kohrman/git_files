5c14aece1eff8c02e791a1f9aa134b0b48febe27
Mark-Kohrman <mark.kohrman@gmail.com>
2020-09-30 19:17:13 -0500

    addding different string exercises

--- whiteboarding_2.rb
+++ whiteboarding_2.rb
@@ -45,17 +45,44 @@
 # string = "b i l l s y a l l g  i  v  e  m  e  t  h  a  t  $"
 #           0 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 
 
-def cash(string)
-  result = false
+# def cash(string)
+#   result = false
+#   index = 0
+#   while index < string.length
+#     if string[index] == "$"
+#       result = true
+#     end
+#     index += 1
+#   end
+#   return result
+# end
+
+# p cash("abcdefghijklmnopqrstuvwxyz")
+
+# Given two strings of equal length, return the number of characters that are different between the two strings.
+
+# Input: "A B C D E F G", "A B C X E O G"
+#         0 1 2 3 4 5 6    0 1 2 3 4 5 6
+# diff    0 0 0 1 0 1 0 
+  
+# Output: 2
+
+# Input: "ABC","BCA"
+# would Output be 3 or 2? 
+
+# input 2 strings
+# loop thru each string simultaneously
+# if index input1 does not equal index input2, add 1 to difference
+def different_strings(string1, string2)
+  result = 0
   index = 0
-  while index < string.length
-    if string[index] == "$"
-      result = true
+  while index < string1.length
+    if string1[index] != string2[index]
+      result += 1
     end
     index += 1
   end
   return result
 end
 
-p cash("abcdefghijklmnopqrstuvwxyz")
-
+p different_strings("ABCDEFGPL", "ABCXEOGIL")
