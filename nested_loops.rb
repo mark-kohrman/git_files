39930589fdf69c6a12af0208882b0eb824dc2041
Mark-Kohrman <mark.kohrman@gmail.com>
2020-10-17 16:21:49 -0500

    adding nested loop product of each number exercise

--- nested_loops.rb
+++ nested_loops.rb
@@ -105,6 +105,20 @@ p sum_of_each
 
 #  7. Use a nested loop with an array of numbers to compute an array with every combination of products from each number.
 #     For example, [2, 8, 3] becomes [4, 16, 6, 16, 64, 24, 6, 24, 9].
+numbers = [2, 8, 3]
+product_numbers = []
+i = 0
+while i < numbers.length
+  i2 = 0
+  first_number = numbers[i]
+  while i2 < numbers.length
+    current_number = numbers[i2]
+    product_numbers << first_number * current_number
+    i2 += 1
+  end
+  i += 1
+end
+p product_numbers
 
 #  8. Use a nested loop to find the largest sum of any two different numbers within an array.
 #     For example, [1, 8, 3, 10] becomes 18.
