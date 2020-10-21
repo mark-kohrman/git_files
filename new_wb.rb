107b7ac48762adbb9ee2813655d658ff800c17f1
Mark-Kohrman <mark.kohrman@gmail.com>
2020-10-20 23:20:18 -0500

    adding loop within loop to get combinations of all letters

--- new_wb.rb
+++ new_wb.rb
@@ -7,28 +7,28 @@
 # Refactor
 
 
+# Given ONE array of strings, return a new array that contains every combination of each string with every other string in the array.
+          # v
+# Input: ["a", "b", "c", "d"]
 
-# Given two arrays of strings, return a new string that contains every combination of a string from the first array concatenated with a string from the second array.
-#               v           v
-# Input: ["a", "b", "c"], ["d", "e", "f", "g"]
+# Output: ["ab", "ac", "ad", "ba", "bc", "bd", "ca", "cb", "cd", "da", "db", "dc"]
 
-# New_Array = ["ad", "ae", "af", "ag", "bd" ]
-#loop within loop to grab first element and concat with with second
+new_array = [""]
 
-
-# Output: ["ad", "ae", "af", "ag", "bd", "be", "bf", "bg", "cd", "ce", "cf", "cg"]
-
-def combine_letters(array1, array2)
+def combinations(array)
   new_array = []
   i = 0
-  while i < array1.length
+  while i < array.length            # array length is 4, i is 0
     i2 = 0
-    while i2 < array2.length
-      new_array << array1[i] + array2[i2]
+    while i2 < array.length         #array length is 4, i2 is 0
+      if i != i2                    #if i doesn't equal i2, then add first index letter to next letter
+        new_array << array[i] + array[i2]  #
+      end
       i2 += 1
     end
     i += 1
   end
-  return new_array
+   return new_array
 end
-p combine_letters(["a", "b", "c"], ["d", "e", "f", "g"])
+
+p combinations(["a", "b", "c", "d"])
