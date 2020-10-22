8a8967779c81f050e475f152cb389d778523cfaf
Mark-Kohrman <mark.kohrman@gmail.com>
2020-10-21 20:19:55 -0500

    adding coolio array exercise

--- new_wb.rb
+++ new_wb.rb
@@ -7,31 +7,55 @@
 # Refactor
 
 
-# Given an array, return a new array that contains the original array’s values in reverse.
+# Given an array of numbers, return true if the array is a "100 Coolio Array", or false if it is not.
 
-          # v
-# Input: [1, 2, 39, 4, 6]
+# A "100 Coolio Array" meets the following criteria:
 
-# Output: [6, 4, 39, 2, 1]
+# Its first and last numbers add up to 100,
+# Its second and second-to-last numbers add up to 100,
+# Its third and third-to-last numbers add up to 100,
+# and so on and so forth.
 
-# new_array = [6, 4, 39, 2, 1]
-# make a new empty array
-#start at the back of my input array
-#input each number from the back of input array into new array
-#make a while loop
-#use length - 1 for index to start at and decrement for index
-#return reversed array
+# Here are examples of 100 Coolio Arrays:
 
-def reverse(array)            #
-  i = array.length - 1        # i = 0
-  reversed_array = []         # reversed_array [6, 4, 39, 2, 1]
+#                  v  v
+#   Input = [1, 2, 3, 97, 98, 99]
+#            0  1  2   3  4    5
+#   Output = true
+#   # if i > i2
+#   # break
+# # 
+# # Input = [90, 20, 70, 100, 30, 80, 10]
+#   Output = true
 
-  while i >= 0                  # 1 >= 0
-    reversed_array << array[i]  #
-    i -= 1
+  # Input = [1, 2, 4, 5, 1]
+  # Output = false
+  # make conditionals to check if first iteration is true
+  # keep use else if any are false
+  #make index and reverse index and stop when they meet each other
+  # may only need one loop'
+
+  def coolio(array)
+    i = 0                          # i is 1
+    i2 = array.length - 1          # i2 is 4
+    result = false                 #result is true
+    while i < i2                   # 1 < 4
+      if array[i] + array[i2] == 100  # 1 +99
+        result = true
+      else
+        result = false
+        break
+      end
+      i += 1
+      i2 -= 1
+    end
+    return result
   end
-  return reversed_array
-end
 
-p reverse([1, 2, 39, 4, 6])
+  p coolio([90, 20, 70, 100, 30, 80, 10])
+
+
+
+
+
 
