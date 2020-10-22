b9ca68ee67dad1b26609fca938369a75e8317aef
Mark-Kohrman <mark.kohrman@gmail.com>
2020-10-22 09:16:17 -0500

    converting array to hash

--- new_wb.rb
+++ new_wb.rb
@@ -7,60 +7,33 @@
 # Refactor
 
 
-# Description
-# Given an array of social media posts and a hash of users, return a list of posts (as an array of hashes) that replaces the submitted_by id number as the person's actual name.
+# You are given two parameters, an array and a number. Return a hash whose keys are each of the values from the array parameter, and whose values are the number parameter.
 
-# For example, given this array of posts (note that the submitted_by is an id number):
+# Input:
 
-# [
-# {title: 'Me Eating Pizza', submitted_by: 231, likes: 1549},
-# {title: 'i never knew how cool i was until now', submitted_by: 989, likes: 3},
-# {title: 'best selfie evar!!!', submitted_by: 111, likes: 1092},
-# {title: 'Mondays are the worst', submitted_by: 403, likes: 644}
-# ]
+# First argument: ["a", "e", "i", "o", "u"]
+# Second argument: 1
+#Make empty hash
+#Loop thru array
+#Set each item equal to 1
+#Print
 
-# And this hash of users (the key is the id number and the value is the user's real name):
+# Output = 
+# {"a"=> 1, "e" => 1, "i" => 1, "o"=> 1, "u" => 1}
 
-# users = {403 => "Aunty Em", 231 => "Joelle P.", 989 => "Lyndon Johnson", 111 => "Patti Q."}
-
-# Return the array of posts as follows:
-
-# [
-# {title: 'Me Eating Pizza', submitted_by: "Joelle P.", likes: 1549},
-# {title: 'i never knew how cool i was until now', submitted_by: "Lyndon Johnson", likes: 3},
-# {title: 'best selfie evar!!!', submitted_by: "Patti Q.", likes: 1092},
-# {title: 'Mondays are the worst', submitted_by: "Aunty Em", likes: 644}
-# ]
-posts = 
-  [
-  {title: 'Me Eating Pizza', submitted_by: 231, likes: 1549},
-  {title: 'i never knew how cool i was until now', submitted_by: 989, likes: 3},
-  {title: 'best selfie evar!!!', submitted_by: 111, likes: 1092},
-  {title: 'Mondays are the worst', submitted_by: 403, likes: 644}
-  ]
-
-users = {403 => "Aunty Em", 231 => "Joelle P.", 989 => "Lyndon Johnson", 111 => "Patti Q."}
-
-new_posts = []
-
-
-### replace value of "submitted_by" with name instead of number
-### use each loop
-## add each post to new array
-## loop through each user and if the id key equals the submitted_by value, replace the id with the value
-
-posts.each do |post|
-  post_id = post[:submitted_by]
-  new_posts <<  post
-  users.each do |id, user_name|
-    if id == post_id
-      name = user_name
-      post[:submitted_by] = name
-    end
+def array_to_hash(array)
+  i = 0
+  new_hash = {}              #new_hash = {"a" => 1, }
+  while i < array.length     # i = 0, length = 5
+    new_hash[array[i]] = 1   #
+    i += 1
   end
+  return new_hash
 end
 
-p new_posts
+p array_to_hash(["a", "e", "i", "o", "u"])
+
+
 
 
 
