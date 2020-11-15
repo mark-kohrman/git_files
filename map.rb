c068430870a6fb394b5a63630f30c7b6fd4a5ca5
Mark-Kohrman <mark.kohrman@gmail.com>
2020-11-15 17:11:39 -0600

    adding problems 3 and 4 for map exercises

--- map.rb
+++ map.rb
@@ -21,9 +21,27 @@ p uppercased_greetings
 
 #  3. Start with an array of hashes and create a new array of string values from each hash's :name key.
 #     For example, [{name: "Alice", age: 27}, {name: "Blane", age: 16}] becomes ["Alice", "Blane"].
+people = [{name: "Alice", age: 27}, {name: "Blane", age: 16}]
+names = []
+i = 0
+while i < people.length
+  names << people[i][:name]
+  i += 1
+end
+p names
 
 #  4. Start with an array of numbers and create a new array with each number plus 7.
 #     For example, [1, 2, 3] becomes [8, 9, 10].
+integers = [4, 5, 9]
+plus_seven = []
+i = 0
+while i < integers.length
+  plus_seven << integers[i] + 7
+  i += 1
+end
+
+p plus_seven
+
 
 #  5. Start with an array of strings and create a new array with each string's length.
 #     For example, ["hello", "goodbye"] becomes [5, 7].
