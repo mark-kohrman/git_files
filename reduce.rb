159f5fa685121d546fb2ca49f4dfe9bb47a79d11
Mark-Kohrman <mark.kohrman@gmail.com>
2020-11-25 16:06:48 -0600

    adding reduce skills exercises

--- reduce.rb
+++ reduce.rb
@@ -2,26 +2,31 @@
 #     For example, [5, 10, 8, 3] becomes 26.
 numbers = [5, 10, 8, 3]
 sum = 0
-numbers.each do |number|
-  sum += number
+i = 0
+while i < numbers.length
+  sum += numbers[i]
+  i += 1
 end
 p sum
+
+
 #  2. Start with an array of strings and combine them all into a single string.
 #     For example, ["volleyball", "basketball", "badminton"] becomes "volleyballbasketballbadminton".
+sports = ["volleyball", "basketball", "badminton"]
+combined_sports = ""
+i = 0
+while i < sports.length
+  combined_sports += sports[i]
+  i += 1
+end
+p combined_sports
 
 #  3. Start with an array of hashes and compute the sum of the prices (from the :price key).
 #     For example, [{name: "chair", price: 100}, {name: "pencil", price: 1}, {name: "book", price: 4}] becomes 105.
 
 #  4. Start with an array of numbers and compute the the minumum number.
 #     For example, [5, 10, 8, 3, 9] becomes 3.
-numbers = [5, 10, 8, 3, 9, 12, 1, 99, -3, 109, 99, 89, -6, 23, 1, 2,31, 3]
-minimum = numbers[0]
-numbers.each do |number|
-  if number < minimum
-    minimum = number
-  end
-end
-p minimum
+
 #  5. Start with an array of strings and compute the total length of all the strings.
 #     For example, ["volleyball", "basketball", "badminton"] becomes 29.
 
@@ -36,23 +41,10 @@ p minimum
 
 #  9. Start with an array of hashes and find the hash with the shortest name (from the :name key).
 #     For example, [{name: "chair", price: 100}, {name: "pencil", price: 1}, {name: "book", price: 4}] becomes {name: "book", price: 4}.
-items =  [{name: "chair", price: 100}, {name: "pen", price: 1}, {name: "book", price: 4}, {name: "rial", price: 32}]
-shortest_length = items[0]
-
-items.each do |item|
-  if item[:name].length < shortest_length[:name].length
-    shortest_length = item
-  end
-end
-p shortest_length
 
 # 10. Start with an array of numbers and compute the maximum number.
 #     For example, [5, 10, 8, 3] becomes 10.
 
-
-# SOLUTIONS (using while loop): https://gist.github.com/peterxjang/376c8931a48549889eb3c9bc091e9f43
-# SOLUTIONS (using .each shortcut): https://gist.github.com/peterxjang/379c9945774f51027750c59d6e4395df
-# SOLUTIONS (using .reduce shortcut): https://gist.github.com/peterxjang/b69183e2d555964ce3936893f423ef35
 
 # SOLUTIONS (using while loop): https://gist.github.com/peterxjang/376c8931a48549889eb3c9bc091e9f43
 # SOLUTIONS (using .each shortcut): https://gist.github.com/peterxjang/379c9945774f51027750c59d6e4395df
